5f28594740456f0b263365dcfd6e889f3a164d84
Tyler Tucker <tylertucker202@gmail.com>
2024-03-28 07:11:04 -1000

    firstcommit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
+
+const main = resolve(__dirname, 'index.tsx')
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  build: {
+    chunkSizeWarningLimit: 1000,
+    outDir: resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: {
+        main: main,
+      },
+    },
+  },
+})
