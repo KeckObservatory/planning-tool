ccd88e9ff35cb9cc6aae26a2d7952d83cf2a2e90
Tyler Tucker <tylertucker202@gmail.com>
2024-11-06 07:26:38 -1000

    adjusting compass rose

--- src/aladin.tsx
+++ src/aladin.tsx
@@ -102,8 +102,8 @@ const get_compass = async (aladin: any, height: number, width: number) => {
 
                 x = x * scale 
                 y = y * scale 
-                x = x - width + margin //shift left
-                y = y - height + margin //shift down
+                // x = x + width + margin //shift left
+                // y = y + height + margin //shift down
 
                 return [x, y] as unknown as Position[]
             })
