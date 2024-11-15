e0ead90d9a2caf3d61aba43d57af63855fec1e66
Tyler Tucker <tylertucker202@gmail.com>
2024-11-14 14:13:08 -1000

    view/edit is a chips input

--- src/target_table.tsx
+++ src/target_table.tsx
@@ -238,7 +238,7 @@ export default function TargetTable() {
         if (changeDetected) {
           const isNumber = type.includes('number') || type.includes('integer')
           const isArray = type.includes('array')
-          value = isArray ? format_tags(value.flat(Infinity)) : format_edit_entry(params.field, value, isNumber)
+          value = (isArray && Array.isArray(value)) ? format_tags(value.flat(Infinity)) : format_edit_entry(params.field, value, isNumber)
           const newTgt = rowSetter(editTarget, params.field, value)
           setEditTarget(newTgt)
         }
