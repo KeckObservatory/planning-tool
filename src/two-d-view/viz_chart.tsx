b81b900da7ecbf7de036bbcac9fa048d686fd445
Tyler Tucker <tylertucker202@gmail.com>
2024-11-25 10:15:30 -1000

    finding the right opacity

--- src/two-d-view/viz_chart.tsx
+++ src/two-d-view/viz_chart.tsx
@@ -345,7 +345,8 @@ export const TargetVizChart = (props: Props) => {
                 color,
                 size: ROUND_MINUTES,
                 symbol: 'square',
-                opacity: 1 // too dense to see ticks
+                //opacity: 1 // too dense to see ticks
+                opacity: .1 // too dense to see ticks
             },
             // color,
             text: texts,
@@ -365,13 +366,14 @@ export const TargetVizChart = (props: Props) => {
     })
 
     if (traces.length > 0) {
-        const UTTrace = {...traces.at(0)} as Partial<Plotly.PlotData>
+        let UTTrace = {...traces.at(0)} as Partial<Plotly.PlotData>
         //@ts-ignore
         const ydates = UTTrace.y.map((date: Date) => dayjs(date).add(10, 'hour').toDate())
         //@ts-ignore
         UTTrace.y = ydates
-        //@ts-ignore
-        // UTTrace.marker?.opacity = 0
+        if (UTTrace.marker) {
+            UTTrace.marker.opacity = 0;
+        }
         UTTrace.yaxis = 'y2'
         UTTrace.visible = true 
         console.log('UTTrace', UTTrace)
