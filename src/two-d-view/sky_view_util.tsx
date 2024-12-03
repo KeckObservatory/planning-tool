939201c3dabc71dd00ddafe5e0de29c86878b5ae
Tyler Tucker <tylertucker202@gmail.com>
2024-12-02 15:10:25 -1000

    moon posiiton converted to degrees

--- src/two-d-view/sky_view_util.tsx
+++ src/two-d-view/sky_view_util.tsx
@@ -208,7 +208,7 @@ const angular_separation = (lon1: number, lat1: number, lon2: number, lat2: numb
     const numerator2 = clat1 * slat2 - slat1 * clat2 * cdlon
     const denominator = slat1 * slat2 + clat1 * clat2 * cdlon
     const numerator = Math.sqrt(numerator1 * numerator1 + numerator2 * numerator2)
-    return Math.atan2(numerator, denominator) * 180 / Math.PI
+    return Math.atan(numerator/denominator) * 180 / Math.PI
 }
 
 export const lunar_angle = (ra: number, dec: number, date: Date, lngLatEl: LngLatEl) => {
