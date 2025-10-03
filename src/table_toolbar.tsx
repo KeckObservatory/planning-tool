e374d2357a9752f7463b02b8ba1a0452c1f5908f
Tyler Tucker <tylertucker202@gmail.com>
2025-10-03 09:01:52 -1000

    expand toolbar padding a little

--- src/table_toolbar.tsx
+++ src/table_toolbar.tsx
@@ -205,7 +205,7 @@ export function EditToolbar(props: EditToolbarProps) {
 
   return (
     // <GridToolbarContainer sx={{ justifyContent: 'center' }}>
-    <GridToolbarContainer sx={{ justifyContent: 'space-between', p: 2 }}>
+    <GridToolbarContainer sx={{ justifyContent: 'space-between', p: 1 }}>
       <Stack justifyContent={'left'} direction="row" spacing={1}>
         <Button color="primary" startIcon={<AddIcon />} onClick={handleAddTarget}>
           Add Target
