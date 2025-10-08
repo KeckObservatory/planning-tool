81cd3004952f6e21fe6ea593176d730a43256e21
Tyler Tucker <tylertucker202@gmail.com>
2025-10-08 11:16:20 -1000

    sorting betta

--- src/two-d-view/sky_chart_data_summary.tsx
+++ src/two-d-view/sky_chart_data_summary.tsx
@@ -136,7 +136,7 @@ export const SkyChartDataSummary = (props: Props) => {
         })
         rows.sort((a, b) => (a["Target Name"] === b["Target Name"]) ? 1 : ((b["Target Name"] === a["Target Name"]) ? -1 : 0))
         rows.sort((a, b) => (a["Datetime (UTC)"] > b["Datetime (UTC)"]) ? 1 : ((b["Datetime (UTC)"] > a["Datetime (UTC)"]) ? -1 : 0))
-        rows = rows.map(r => ({ ...r, "Datetime (UTC)": r["Datetime (UTC)"].toUTCString().replace('T', ' ').split('.')[0] })) //remove ms
+        rows = rows.map(r => ({ ...r, "Datetime (UTC)": r["Datetime (UTC)"].toISOString().replace('T', ' ').split('.')[0] })) //remove ms
         return rows
     }
 
