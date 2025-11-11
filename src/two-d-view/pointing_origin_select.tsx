19ef5e31528b7323bc66a920990468283fdfdcd5
Tyler Tucker <tylertucker202@gmail.com>
2025-11-11 08:17:02 -1000

    po to 2dview

--- src/two-d-view/pointing_origin_select.tsx
+++ src/two-d-view/pointing_origin_select.tsx
@@ -1,8 +1,12 @@
-import { Autocomplete, TextField, Tooltip } from "@mui/material"
+import { Autocomplete, Checkbox, TextField, Tooltip } from "@mui/material"
 import React from "react"
 import { useEffect } from "react"
 import { StringParam, useQueryParam, withDefault } from "use-query-params"
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
 
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
 
 interface POSelectProps {
     pointing_origins?: GeoJSON.FeatureCollection<GeoJSON.Point>
@@ -11,12 +15,16 @@ interface POSelectProps {
 
 export const POSelect = (props: POSelectProps) => {
     const { pointing_origins, instrument } = props
-    const [ pointingOrigin, setPointingOrigin ] = useQueryParam<string>('pointing_origin', withDefault(StringParam, ''))
-    const [ options, setOptions ] = React.useState<string[]>([])
+    const [pointingOrigin, setPointingOrigin] = React.useState<GeoJSON.Feature<GeoJSON.Point>[]>([])
+    const [options, setOptions] = React.useState<GeoJSON.Feature<GeoJSON.Point>[]>([])
 
-    const onPointingOriginChange = (value?: string ) => {
+    const onPointingOriginChange = (value?: string[]) => {
         if (value) {
-            setPointingOrigin(value)
+            setPointingOrigin(value.map((name) => options.find((option) => option.properties?.name === name))
+            .filter((option): option is GeoJSON.Feature<GeoJSON.Point> => option !== undefined) as GeoJSON.Feature<GeoJSON.Point>[])
+        }
+        else {
+            setPointingOrigin([])
         }
     }
 
@@ -27,22 +35,41 @@ export const POSelect = (props: POSelectProps) => {
             return
         }
         const filteredOptions = pointing_origins.features
-        .filter((feature) => instrument.includes(feature.properties?.instrument) )
+            .filter((feature) => instrument.includes(feature.properties?.instrument))
             .map((feature) => feature.properties?.name ?? '')
-        setOptions(filteredOptions)
+        setOptions([undefined, ...filteredOptions])
     }, [pointing_origins, instrument])
-
 
     return (
         <Tooltip placement="top" title="Select pointing origin">
             <Autocomplete
                 disablePortal
                 id="pointing-origin-selection"
-                value={{ label: pointingOrigin }}
-                onChange={(_, value) => onPointingOriginChange(value?.label)}
-                options={options.map((name) => ({ label: name }))}
+                value={pointingOrigin.map((feature) => feature.properties?.name ?? '')}
+                onChange={(_, value) => onPointingOriginChange(value)}
+                options={options.map( (feature) => {
+                        return { title: feature.properties?.name ?? '', label: feature.properties?.name ?? '' }
+                    })}
                 sx={{ width: '200px', paddingTop: '9px', margin: '6px' }}
                 renderInput={(params) => <TextField {...params} label="PO" />}
+                multiple
+                disableCloseOnSelect
+                getOptionLabel={(option) => option.label}
+                renderOption={(props, option, { selected }) => {
+                    const { key, ...optionProps } = props;
+                    return (
+                        <li key={key} {...optionProps}>
+                            <Checkbox
+                                icon={icon}
+                                checkedIcon={checkedIcon}
+                                style={{ marginRight: 8 }}
+                                checked={selected}
+                            />
+                            {option.label}
+                        </li>
+                    );
+                }}
+                style={{ width: 500 }}
             />
         </Tooltip>
     )
