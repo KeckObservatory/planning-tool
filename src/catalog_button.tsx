fa38abf88b875507d3337a36f062908084c74e69
Tyler Tucker <tylertucker202@gmail.com>
2026-02-09 10:40:33 -1000

    unit conversion

--- src/catalog_button.tsx
+++ src/catalog_button.tsx
@@ -108,8 +108,8 @@ export const get_simbad_data = async (targetName: string): Promise<SimbadTargetD
         }
         else if (line.startsWith('Proper motions')) {
             const [pmRa, pmDec] = line.split(' ').slice(2, 4);
-            Number(pmRa) && (simbadData['pm_ra'] = Number(pmRa))
-            Number(pmDec) && (simbadData['pm_dec'] = Number(pmDec))
+            Number(pmRa) && (simbadData['pm_ra'] = Number(pmRa)/1000) //convert from mas/yr to arcsec/yr
+            Number(pmDec) && (simbadData['pm_dec'] = Number(pmDec)/1000) //convert from mas/yr to arcsec/yr
         }
         else if (identifiersSection) //only check if in identifiers section
         {
