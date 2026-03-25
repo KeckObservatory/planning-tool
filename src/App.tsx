5378e86463bf42a0278c67877ae60da4f36b9327
Tyler Tucker <tylertucker202@gmail.com>
2026-03-24 14:15:39 -1000

    no longer need mock items

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ import Skeleton from '@mui/material/Skeleton';
 import { get_targets, get_userinfo } from './api/api_root.tsx';
 import { SimbadTargetData } from './catalog_button.tsx';
 import { config } from './config.tsx';
-import { mockTargets } from './mock_targets.tsx';
 
 export type Status = "EDITED" | "CREATED"
 
