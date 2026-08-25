d6f6e181488b184aff8298b26c960c2a44810969
Tyler Tucker <tylertucker202@gmail.com>
2026-08-24 16:07:10 -1000

    invert mag range

--- src/guide_star/mag_range_slider.tsx
+++ src/guide_star/mag_range_slider.tsx
@@ -40,10 +40,10 @@ export const MagRangeSlider = (props: Props) => {
             <Slider
                 getAriaLabel={() => 'Temperature range'}
                 value={value}
-                min={MAG_RANGE[0]}
+                min={MAG_RANGE[1]}
                 step={.2}
                 disabled={props.disabled}
-                max={MAG_RANGE[1]}
+                max={MAG_RANGE[0]}
                 onChange={handleChange}
                 onChangeCommitted={handleChangeCommitted}
                 valueLabelDisplay="auto"
