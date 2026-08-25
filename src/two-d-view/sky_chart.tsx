a457687ee09efecd85067b9935dd756cb03f2d76
Tyler Tucker <tylertucker202@gmail.com>
2026-08-24 15:43:15 -1000

    tag length does not control tooltip size

--- src/two-d-view/sky_chart.tsx
+++ src/two-d-view/sky_chart.tsx
@@ -57,7 +57,7 @@ export const generateData = (tgtv: TargetView,
         chartType.includes('Lunar Angle') && (txt += `Moon Fraction: ${viz.moon_illumination.fraction.toFixed(2)}<br>`)
         txt += `Visible for: ${tgtv.visibilitySum.toFixed(2)} hours<br>`
         txt += viz.observable ? '' : `<br>Not Observable: ${viz.reasons.join(', ')}`
-        txt += tgtv.tags ? `<br>Tags: ${tgtv.tags.join(', ')}` : ''
+        txt += tgtv.tags && tgtv.tags.length > 0 ? `<br>Tags:<br>${tgtv.tags.join('<br>')}` : ''
         txt += tgtv.comment ? `<br>Notes: ${tgtv.comment}` : ''
         const datum = viz.moon_position ? util.get_chart_datum(tgtv.ra_deg, tgtv.dec_deg, viz, chartType, lngLatEl) : null
         data.push({
